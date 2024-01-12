e5f170468f5092c89ab5fbb5c1ce933fd5b93532
Victor-python <64462520+Victor-python@users.noreply.github.com>
2024-01-12 14:17:00 +0200

    asdasd

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express();
 app.use("/", (req, res) => {
   res.send("server running");
 });
+app.get("/", (req, res) => {
+  res.send("server running");
+});
 
 app.listen(PORT, () => {
   console.log(`server running on http://localhost:${PORT}`);
